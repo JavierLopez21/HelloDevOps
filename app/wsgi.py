79df438ae8be7be3fcb1b5ab8ccdd1361c9be715
JavierLopez21 <44420416+JavierLopez21@users.noreply.github.com>
2023-03-31 09:55:36 -0600

    Actualización

--- app/wsgi.py
+++ app/wsgi.py
@@ -3,8 +3,8 @@ import pandas as pd
 import json
 import plotly
 import plotly.express as px
-import hvac
 import os
+import hvac
 
 app = Flask(__name__)
 
@@ -17,12 +17,14 @@ def notdash():
    df2 = df[['state','active']].groupby('state').sum().reset_index().rename({'active':'total'},axis=1).sort_values(by='total').iloc[:10,:]
    fig = px.bar(df2, x='state', y='total',color='total',title='Densidad de negocios activos por estado')
    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
-   try:
-      client = hvac.Client(url='http://vault:8200')
-      client.token = 'myroot'
-      secret = 'La conexión fue exitosa'
-   except:
+
+   client = hvac.Client(url='http://vault:8200', token="root")
+   if client.sys.is_initialized():
+      secret = client.secrets.kv.v2.read_secret_version(path='myapp/config')
+      secret = secret['data']
+   else:
       secret = 'La conexión falló'
+   
 
    
    return render_template('index.html', graphJSON=graphJSON, line = secret)
--- src/credentials.py
+++
@@ -1,6 +0,0 @@
-credentials = {"user":"root",
- "pwd":"12345",
- "host":"127.0.0.1",
- "port":"3306",
- "db":"hello5"
-}
